d04d827a0e432fc94f207ab0691e4cdbc09f8f14
Hogeyama <gan13027830@gmail.com>
2019-07-06 18:32:04 +0900

    Improve message

--- lib/abstraction/hflmc2_abstraction.ml
+++ lib/abstraction/hflmc2_abstraction.ml
@@ -40,134 +40,131 @@ let merge_lambda : int -> (Hfl.t list -> Hfl.t) -> Hfl.t list -> Hfl.t =
 (* Γ |- σ <= σ ↪ φ' で得たφ'にφを適用 *)
 let rec abstract_coerce : env -> abstraction_ty -> abstraction_ty -> Hfl.t -> Hfl.t =
   fun env sigma sigma' phi ->
-    let debug = false in
-    if debug then begin
-      Print.pr "==================@.";
-      Print.print ~tag:"σ " Print.abstraction_ty sigma;
-      Print.print ~tag:"σ'" Print.abstraction_ty sigma';
-    end;
-    match sigma, sigma' with
-    | TyBool ps, TyBool qs ->
-        (* x[j] is a variable for q[j] *)
-        let xs = List.init (List.length qs) ~f:(fun _ -> Id.gen ATyBool) in
-        begin
-        try
-          (* Simple case: forall p[i], there exists j_i s.t. p[i] = q[j_i] *)
-          let qxs = List.zip_exn qs xs in
-          (* gather x[j_i] *)
-          let pxs = List.map ps ~f:begin fun p ->
-              snd @@ List.find_exn qxs ~f:(fun (q,_) -> FpatInterface.(p <=> q))
-            end
-          in
-          (* assemble *)
-          let phi' = Hfl.mk_abss xs @@ Hfl.mk_apps phi (List.map ~f:Hfl.mk_var pxs) in
-          phi'
-        with Not_found ->
-          if false then begin
-            Print.print ~tag:"ps" Print.(list_comma formula) ps;
-          end;
-          (* Let ps be P1,...,Pk and qs be Q1,...,Ql.
-           * To compute φ, find I ⊆ {1,...,l} and J1,...,Jm ⊆ {1,...,k} such that
-           *    ∧ _{i \in I}Qi => ∨ _{h \in 1,...,m}∧ _{j \in Jh} Pj
-           * Let
-           *    φ'_{I,J1,...,Jm} = ∧ _{i \in I}b_i ∧ _{h \in 1,...,m} φ (1 \in Jh) ... (l \in Jh)
-           * then φ' is the union of all φ'_{I,J1,...,Jm}.
-           * *)
-          let l = List.length qs in
-          let k = List.length ps in
-          let max_ors = !Option.Abstraction.max_ors in
-          let one_to_l = List.(range ?start:(Some `inclusive) ?stop:(Some `exclusive) 0 l) in
-          let one_to_k = List.(range ?start:(Some `inclusive) ?stop:(Some `exclusive) 0 k) in
-          let _Is = List.powerset one_to_l in
-          let _Js =
-            List.filter (List.powerset one_to_k) ~f:begin fun _J ->
-              let ps' = List.(map ~f:(nth_exn ps) _J) in
-              FpatInterface.is_consistent_set ps' && not (List.is_empty _J)
-            end
-          in
-          let _Jss =
-            List.filter (List.powerset ~limit:max_ors _Js)
-              ~f:(fun l -> not (List.is_empty l))
-          in
-          let phi's =
-            List.map _Is ~f:begin fun _I ->
-              let xs' = List.(map ~f:(nth_exn xs) _I) in
-              let qs' = List.(map ~f:(nth_exn qs) _I) in
-              let _Q  = Formula.mk_ands qs' in
+    let phi' =
+      match sigma, sigma' with
+      | TyBool ps, TyBool qs ->
+          (* x[j] is a variable for q[j] *)
+          let xs = List.init (List.length qs) ~f:(fun _ -> Id.gen ATyBool) in
+          begin
+          try
+            (* Simple case: forall p[i], there exists j_i s.t. p[i] = q[j_i] *)
+            let qxs = List.zip_exn qs xs in
+            (* gather x[j_i] *)
+            let pxs =
+              List.map ps ~f:begin fun p ->
+                snd @@ List.find_exn qxs ~f:(fun (q,_) -> FpatInterface.(p <=> q))
+              end
+            in
+            (* assemble *)
+            Hfl.mk_abss xs @@ Hfl.mk_apps phi (List.map ~f:Hfl.mk_var pxs)
+          with Not_found ->
+            (* Let ps be P1,...,Pk and qs be Q1,...,Ql.
+             * To compute φ, find I ⊆ {1,...,l} and J1,...,Jm ⊆ {1,...,k} such that
+             *    ∧ _{i \in I}Qi => ∨ _{h \in 1,...,m}∧ _{j \in Jh} Pj
+             * Let
+             *    φ'_{I,J1,...,Jm} = ∧ _{i \in I}b_i ∧ _{h \in 1,...,m} φ (1 \in Jh) ... (l \in Jh)
+             * then φ' is the union of all φ'_{I,J1,...,Jm}.
+             * *)
+            let l = List.length qs in
+            let k = List.length ps in
+            let max_ors = !Option.Abstraction.max_ors in
+            let one_to_l = List.(range ?start:(Some `inclusive) ?stop:(Some `exclusive) 0 l) in
+            let one_to_k = List.(range ?start:(Some `inclusive) ?stop:(Some `exclusive) 0 k) in
+            let _Is = List.powerset one_to_l in
+            let _Js =
+              List.filter (List.powerset one_to_k) ~f:begin fun _J ->
+                let ps' = List.(map ~f:(nth_exn ps) _J) in
+                FpatInterface.is_consistent_set ps' && not (List.is_empty _J)
+              end
+            in
+            let _Jss =
+              List.filter (List.powerset ~limit:max_ors _Js)
+                ~f:(fun l -> not (List.is_empty l))
+            in
+            let phi's =
+              List.map _Is ~f:begin fun _I ->
+                let xs' = List.(map ~f:(nth_exn xs) _I) in
+                let qs' = List.(map ~f:(nth_exn qs) _I) in
+                let _Q  = Formula.mk_ands qs' in
 
-              (* /\Q => \/i(/\Ji) を満たす極大の J1,...,Jh の集合を得る *)
-              let ans =
-                if FpatInterface.(_Q ==> Formula.Bool false)
-                then
-                  [[one_to_k]] (* /\{P1,...,Pk}が唯一の極大元 *)
-                else if
-                  (* See [FpatInterface.strongest_post_cond'] *)
-                  FpatInterface.(Formula.Bool true ==> _Q) || !exhaustive_search
-                then
-                  let candidates : (int list list * Formula.t) list =
-                    List.filter_map _Jss ~f:begin fun _Js ->
-                      let pss =
-                            List.map _Js ~f:begin fun _J ->
-                              List.map _J ~f:begin fun j ->
-                                List.nth_exn ps j end end
-                      in
-                      let body =
-                            Formula.mk_ors @@ List.map ~f:Formula.mk_ands pss
-                      in
-                      if FpatInterface.(_Q ==> body)
-                      then Some (_Js, body)
-                      else None
-                    end
-                  in
-                  if true (* もしかしたらバグってるかも知れない *)
+                (* /\Q => \/i(/\Ji) を満たす極大の J1,...,Jh の集合を得る *)
+                let ans =
+                  if FpatInterface.(_Q ==> Formula.Bool false)
                   then
+                    [[one_to_k]] (* /\{P1,...,Pk}が唯一の極大元 *)
+                  else if
+                    (* See [FpatInterface.strongest_post_cond'] *)
+                    FpatInterface.(Formula.Bool true ==> _Q) || !exhaustive_search
+                  then
+                    let candidates : (int list list * Formula.t) list =
+                      List.filter_map _Jss ~f:begin fun _Js ->
+                        let pss =
+                              List.map _Js ~f:begin fun _J ->
+                                List.map _J ~f:begin fun j ->
+                                  List.nth_exn ps j end end
+                        in
+                        let body =
+                              Formula.mk_ors @@ List.map ~f:Formula.mk_ands pss
+                        in
+                        if FpatInterface.(_Q ==> body)
+                        then Some (_Js, body)
+                        else None
+                      end
+                    in
+                    (* XXX 本当にこれで弱くなってない？ *)
                     let (<=) (_,p1) (_,p2) = FpatInterface.(p2 ==> p1) in
                     List.map ~f:fst @@ Fn.maximals' (<=) candidates
                   else
-                    List.map ~f:fst candidates
-                else
-                  [FpatInterface.strongest_post_cond _Q ps]
-              in
-              let nodes = List.map ans ~f:begin fun _Js ->
-                  let mk_atom _J =
-                        Hfl.mk_apps phi @@
-                          List.map one_to_k ~f:begin fun j ->
-                            Hfl.Bool (List.mem ~equal:(=) _J j)
-                          end
-                  in
-                  Hfl.mk_ands ~kind:`Inserted
-                      @@ Hfl.mk_ands ~kind:`Inserted (List.map xs' ~f:Hfl.mk_var)
-                      :: List.map _Js ~f:mk_atom
-                end
-              in
-              (* log *)
-              if true then List.iter ans ~f:begin fun _Js ->
-                let pss =
-                      List.map _Js ~f:begin fun _J ->
-                        List.map _J ~f:begin fun j ->
-                          List.nth_exn ps j end end
+                    [FpatInterface.strongest_post_cond _Q ps]
                 in
-                Log.debug begin fun m -> m ~header:"Coerce" "I = %a,@ J = %a"
-                  Print.(list_set formula) qs'
-                  Print.(list_set (list_set formula)) pss
+                let nodes =
+                  List.map ans ~f:begin fun _Js ->
+                    let mk_atom _J =
+                          Hfl.mk_apps phi @@
+                            List.map one_to_k ~f:begin fun j ->
+                              Hfl.Bool (List.mem ~equal:(=) _J j)
+                            end
+                    in
+                    Hfl.mk_ands ~kind:`Inserted
+                        @@ Hfl.mk_ands ~kind:`Inserted (List.map xs' ~f:Hfl.mk_var)
+                        :: List.map _Js ~f:mk_atom
+                  end
+                in
+                (* log *)
+                List.iter ans ~f:begin fun _Js ->
+                  let pss =
+                        List.map _Js ~f:begin fun _J ->
+                          List.map _J ~f:begin fun j ->
+                            List.nth_exn ps j end end
+                  in
+                  Log.debug begin fun m -> m ~header:"Coerce" "I = %a,@ J = %a"
+                    Print.(list_set formula) qs'
+                    Print.(list_set (list_set formula)) pss
+                  end;
                 end;
-              end;
-              Hfl.mk_ors ~kind:`Inserted nodes
-            end
-          in
-          let phi' = Hfl.mk_abss xs @@ Hfl.mk_ors ~kind:`Inserted phi's in
-          phi'
-        end
-    | TyArrow({ty = TyInt; _} as x, sigma), TyArrow({ty = TyInt; _} as y, sigma') ->
-        let sigma = Subst.Arith.abstraction_ty x (Arith.mk_var y) sigma in
-        abstract_coerce env sigma sigma' phi
-    | TyArrow({ty = TySigma sigma1 ; _}, sigma2 )
-    , TyArrow({ty = TySigma sigma1'; _}, sigma2') ->
-        let f = Id.gen ~name:"ac_f" (Type.abstract sigma) in
-        let x = Id.gen ~name:"ac_x" (Type.abstract sigma1') in
-        let phi1x = abstract_coerce env sigma1' sigma1 (Var x) in
-        abstract_coerce env sigma2 sigma2' @@ App(Var f, phi1x)
-    | _ -> assert false
+                Hfl.mk_ors ~kind:`Inserted nodes
+              end
+            in
+            Hfl.mk_abss xs @@ Hfl.mk_ors ~kind:`Inserted phi's
+          end
+      | TyArrow({ty = TyInt; _} as x, sigma), TyArrow({ty = TyInt; _} as y, sigma') ->
+          let sigma = Subst.Arith.abstraction_ty x (Arith.mk_var y) sigma in
+          abstract_coerce env sigma sigma' phi
+      | TyArrow({ty = TySigma sigma1 ; _}, sigma2 )
+      , TyArrow({ty = TySigma sigma1'; _}, sigma2') ->
+          let f = Id.gen ~name:"ac_f" (Type.abstract sigma) in
+          let x = Id.gen ~name:"ac_x" (Type.abstract sigma1') in
+          let phi1x = abstract_coerce env sigma1' sigma1 (Var x) in
+          abstract_coerce env sigma2 sigma2' @@ App(Var f, phi1x)
+      | _ -> assert false
+    in
+    Log.debug begin fun m -> m ~header:"Term:Coerce" "@[<hv 0>%a@;<1 1>: %a ≺  %a@;<1 0>⇢  %a@]"
+      Print.hfl phi
+      Print.abstraction_ty sigma
+      Print.abstraction_ty sigma'
+      Print.hfl phi'
+    end;
+    phi'
 
 let rec abstract_infer : env -> simple_ty Hflz.t -> Type.abstraction_ty * Hfl.t =
   fun env psi ->
@@ -231,12 +228,14 @@ let rec abstract_infer : env -> simple_ty Hflz.t -> Type.abstraction_ty * Hfl.t 
           in
           let phi' = merge_lambda (List.length preds') make_ope phis' in
           (sigma', phi')
-      | Exists _ | Forall _ -> assert false (* unimplemented *)
-      | Abs _ | Arith _ -> assert false (* impossible *)
+      | Exists _ | Forall _ -> Fn.todo()
+      | Abs _ | Arith _ ->
+          Print.(print (hflz simple_ty_) psi);
+          assert false (* impossible *)
           (* NOTE Absはpsiがβ-reduxを持たないという仮定の元でimpossible *)
     in
-      let phi = Simplify.hfl ~force:false phi in
-      Log.debug begin fun m -> m ~header:"Term" "@[<hv 0>%a@ ==> %a@;<1 1>⇢  %a@]"
+      let phi = Simplify.hfl phi in
+      Log.debug begin fun m -> m ~header:"Term:Infer" "@[<hv 0>%a@ ==> %a@;<1 1>⇢  %a@]"
         Print.(hflz simple_ty_) psi
         Print.abstraction_ty sigma
         Print.hfl phi
@@ -258,7 +257,7 @@ and abstract_check : env -> simple_ty Hflz.t -> Type.abstraction_ty -> Hfl.t =
           abstract_coerce env sigma' sigma phi
     in
       let phi = Simplify.hfl ~force:false phi in
-      Log.debug begin fun m -> m ~header:"Term" "@[<hv 0>%a@ <== %a@;<1 1>⇢  %a@]"
+      Log.debug begin fun m -> m ~header:"Term:Check" "@[<hv 0>%a@ <== %a@;<1 1>⇢  %a@]"
         Print.(hflz simple_ty_) psi
         Print.abstraction_ty sigma
         Print.hfl phi
@@ -308,7 +307,7 @@ let abstract : env -> simple_ty Hflz.hes -> Hfl.hes =
               }
         in
         begin Log.debug @@ fun m -> m ~header:"Nonterminal" "%a"
-            Print.hfl_hes_rule main'
+          Print.hfl_hes_rule main'
         end;
         main' :: List.map ~f:(abstract_rule env) hes
     | [] -> assert false
