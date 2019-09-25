a208c685b939d307df975c8f5bae63ff73b8b394
Hogeyama <gan13027830@gmail.com>
2019-09-26 01:47:40 +0900

    Reimpl FpatInterface.PredAbst by myself

--- lib/abstraction/fpatInterface.ml
+++ lib/abstraction/fpatInterface.ml
@@ -16,10 +16,12 @@ let () =
 
 (* Boolean formula *)
 type bvar = (string * [`Pos|`Neg])
+  [@@deriving eq,ord,show,iter,map,fold,sexp]
 let negate_var (x, p) : bvar = match p with
   | `Pos -> (x, `Neg)
   | `Neg -> (x, `Pos)
 type bformula = (bvar, Void.t) Formula.gen_t
+  [@@deriving eq,ord,show,iter,map,fold,sexp]
 
 let rec of_arith : Arith.t -> Fpat.Formula.t = function
   | Int n -> Fpat.Formula.of_term @@ Fpat.Term.mk_const (Fpat.Const.Int n)
@@ -90,42 +92,320 @@ let ( <=> ) : Formula.t -> Formula.t -> bool =
 
 let is_valid : Formula.t -> bool =
   fun f -> Formula.Bool true ==> f
+let is_unsat : Formula.t -> bool =
+  fun f -> f ==> Formula.Bool false
 
 let is_consistent_set : Formula.t list -> bool =
   fun fs -> not (Formula.mk_ands fs ==> Formula.Bool false)
 
+module FpatReImpl = struct
+  type def =
+    { pred : Formula.t
+    ; bvar : bvar }
+    [@@deriving show,iter,map,fold,sexp]
+  let equal_def   def1 def2 = String.equal   (fst def1.bvar) (fst def2.bvar)
+  let compare_def def1 def2 = String.compare (fst def1.bvar) (fst def2.bvar)
+
+  type defs = def array
+
+  (* TODO intのbitを使う *)
+  module Conj_ = struct
+    (* represents conjunction of predicates 
+     * If defs=[|p1;p2;p3|], [|true;false;true|} represents p1/\p3 *)
+    type t = bool array
+    let sexp_of_t = Array.sexp_of_t Bool.sexp_of_t
+    let t_of_sexp = Array.t_of_sexp Bool.t_of_sexp
+    let compare   = Array.compare Bool.compare
+
+    let singleton len i =
+      let pbs = Array.create ~len false in
+      pbs.(i) <- true;
+      pbs
+    let conj pbs pbs' =
+      Array.map2_exn pbs pbs' ~f:begin fun b1 b2 -> b1 || b2 end
+
+    let value : defs -> t -> Formula.t =
+      fun defs elt ->
+        Array.fold2_exn defs elt ~init:(Formula.Bool true) ~f:begin fun acc pb b ->
+          if b
+          then Formula.mk_and acc pb.pred
+          else acc
+        end
+
+    let bvalue : defs -> t -> bformula =
+      fun defs elt ->
+        Array.fold2_exn defs elt ~init:(Formula.Bool true) ~f:begin fun acc pb b ->
+          if b
+          then Formula.mk_and acc (Formula.mk_var pb.bvar)
+          else acc
+        end
+  end
+  module Conj = struct
+    type t = int
+    let sexp_of_t = sexp_of_int
+    let t_of_sexp = int_of_sexp
+    let compare   = compare
+    let pp =
+      let bin_of_int d =
+        if d < 0 then invalid_arg "bin_of_int" else
+        if d = 0 then "0" else
+        let rec aux acc d =
+          if d = 0 then acc else
+          aux (string_of_int (d land 1) :: acc) (d lsr 1)
+        in String.concat (aux [] d)
+      in fun ppf x ->
+        Print.pf ppf "%8s" (bin_of_int x)
+
+    let conj x y = x lor y
+
+    let (=~>) x y = lnot ((lnot y) lor x) = 0 (* trivially x ⇒ y *)
+
+    let singleton _len i = 1 lsl i
+
+    let value : defs -> t -> Formula.t =
+      fun defs elt ->
+        Formula.mk_ands @@ List.init (Array.length defs) ~f:begin fun i ->
+          if elt land (1 lsl i) <> 0
+          then defs.(i).pred
+          else Formula.Bool true
+        end
+
+    let bvalue : defs -> t -> bformula =
+      fun defs elt ->
+        Formula.mk_ands @@ List.init (Array.length defs) ~f:begin fun i ->
+          if elt land (1 lsl i) <> 0
+          then Formula.mk_var defs.(i).bvar
+          else Formula.Bool true
+        end
+  end
+
+  let imply defs env elt phi =
+    Formula.mk_and env (Conj.value defs elt) ==> phi
+
+   let (=~>) pbs1 pbs2 = (* trivially pbs1 ⇒ pbs2 *)
+     Array.for_all2_exn pbs1 pbs2 ~f:begin fun b1 b2 ->
+       (not b2 || b1)
+     end
+
+  module Pbss = Set.Make(struct
+    include Conj
+    include Core.Comparable.Make(Conj)
+  end)
+  module PbssAsKey = struct
+    include Pbss
+    include Core.Comparable.Make(Pbss)
+  end
+
+  type pbss = Pbss.t
+
+  type guard = Formula.t
+
+  let pp_pbss ppf pbss =
+    Print.pf ppf "%a" Print.(list_set Conj.pp) @@ Pbss.to_list pbss
+
+  let weakest defs env phi pbss1' pbss2' pbss3' pbss =
+    let phi = Trans.Simplify.formula phi in
+    Log.debug begin fun m -> m ~header:"defs" "@[<v>%a@]"
+      begin
+        let item ppf (f,i) = Print.pf ppf "%d --> %a" i Print.formula f.pred in
+        Print.list item
+      end begin
+        Array.to_list @@ Array.mapi defs ~f:(fun i f -> (f,i))
+      end
+    end;
+    Log.debug begin fun m -> m ~header:"phi" "@[<v>%a@]"
+      Print.formula phi
+    end;
+    let rec go pbss1' pbss2' pbss3' pbss =
+      Log.debug begin fun m -> m ~header:"Input:pbss" "@[<v>%a@]"
+        pp_pbss pbss
+      end;
+      Log.debug begin fun m -> m ~header:"Input:pbssN'"
+        "@[<v>pbss1': %a@,pbss2': %a@,pbss3': %a@]"
+          pp_pbss pbss1'
+          pp_pbss pbss2'
+          pp_pbss pbss3'
+      end;
+      let pbss1, pbss2, pbss3 =
+        let pbss1, rest =
+          Pbss.partition_tf pbss ~f:begin fun pbs ->
+            imply defs env pbs phi
+          end
+        in
+        let pbss2, pbss3 =
+          Pbss.partition_tf rest ~f:begin fun pbs ->
+            not @@ imply defs env pbs (Formula.mk_not phi)
+          end
+        in pbss1, pbss2, pbss3
+      in
+      Log.debug begin fun m -> m ~header:"Process:pbssN"
+        "@[<v>pbss1:  %a@,pbss2:  %a@,pbss3:  %a@]"
+          pp_pbss pbss1
+          pp_pbss pbss2
+          pp_pbss pbss3
+      end;
+      let pbss1, pbss2, pbss3 =
+        Pbss.union pbss1 pbss1',
+        Pbss.union pbss2 pbss2',
+        Pbss.union pbss3 pbss3'
+      in
+        Set.map (module PbssAsKey) pbss2 ~f:begin fun pbs ->
+          Pbss.map pbss2 ~f:begin fun pbs' ->
+            Conj.conj pbs pbs'
+          end
+        end
+        |> Set.to_list
+        |> Pbss.union_list
+        |> Set.diff -$- pbss2 (* TODO これでいいのか *)
+        (* |> Pbss.filter ~f:begin fun pbs -> *)
+        (*      Array.count pbs ~f:Fn.id <= 3 (* andは3つまで *) *)
+        (*    end *)
+        |> begin fun pbss ->
+            Log.debug begin fun m -> m ~header:"npbss:1" "%a"
+              pp_pbss pbss
+            end; pbss
+           end
+        |> Pbss.filter ~f:begin fun pbs -> (* これは停止性に必須っぽい *)
+             (* pbs2 \in pbss2 は pbs2=>phi も pbs2=>¬phi も満たさない
+              * pbsがpbs2のsubsetならなおさら *)
+             not (Pbss.exists ~f:Conj.(fun pbs' -> pbs' =~> pbs) pbss2) &&
+             (* 今度は逆に pbs=>pbs1=>phi, pbs=>pbs3=>¬phi が自明に成り立つので *)
+             not (Pbss.exists ~f:Conj.(fun pbs' -> pbs =~> pbs') pbss1) &&
+             not (Pbss.exists ~f:Conj.(fun pbs' -> pbs =~> pbs') pbss3) &&
+             true
+           end
+        |> begin fun pbss ->
+            Log.debug begin fun m -> m ~header:"npbss:2" "%a"
+              pp_pbss pbss
+            end; pbss
+           end
+        (* |> Fn.maximals' list_subset *) (* TODO *)
+        |> begin fun pbss ->
+            if Pbss.is_empty pbss
+            then (pbss1, pbss3)
+            else go pbss1 pbss2 pbss3 pbss
+           end
+    in go pbss1' pbss2' pbss3' pbss
+  let weakest env defs phi : bformula =
+    let len = Array.length defs in
+    let pbss1, _pbss3 =
+      weakest
+        defs
+        env
+        phi
+        Pbss.empty (* pbss1' *)
+        Pbss.empty (* pbss2' *)
+        Pbss.empty (* pbss3' *)
+        (Pbss.of_list (List.init len ~f:(Conj.singleton len)))
+    in
+    Pbss.to_list pbss1
+    |> List.filter ~f:(fun x -> not (is_unsat (Conj.value defs x)))
+    |> List.map ~f:(Conj.bvalue defs)
+    |> Formula.mk_ors
+
+  let min_unsat_cores defs env pbss1' pbss2' pbss =
+    let rec go pbss1' pbss2' pbss =
+      let pbss1, pbss2 =
+        Pbss.partition_tf pbss ~f:begin fun pbs ->
+          imply defs env pbs (Formula.Bool false)
+        end
+      in
+      let pbss1, pbss2 =
+        Pbss.union pbss1 pbss1',
+        Pbss.union pbss2 pbss2'
+      in
+        Set.map (module PbssAsKey) pbss2 ~f:begin fun pbs ->
+          Pbss.map pbss2 ~f:begin fun pbs' ->
+            Conj.conj pbs pbs'
+          end
+        end
+        |> Set.to_list
+        |> Pbss.union_list
+        |> Set.diff -$- pbss2 (* TODO これでいいのか *)
+        (* |> Pbss.filter ~f:begin fun pbs -> *)
+        (*      Array.count pbs ~f:Fn.id <= 3 (* andは3つまで *) *)
+        (*    end *)
+        |> Pbss.filter ~f:begin fun pbs -> (* これは停止性に必須っぽい *)
+             (* pbs2 \in pbss2 は pbs2=>phi も pbs2=>¬phi も満たさない
+              * pbsがpbs2のsubsetならなおさら *)
+             not (Pbss.exists ~f:Conj.(fun pbs' -> pbs' =~> pbs) pbss2) &&
+             (* 今度は逆に pbs=>pbs1=>phi, pbs=>pbs3=>¬phi が自明に成り立つので *)
+             not (Pbss.exists ~f:Conj.(fun pbs' -> pbs =~> pbs') pbss1) &&
+             true
+           end
+        |> begin fun pbss ->
+            if Pbss.is_empty pbss
+            then pbss1
+            else go pbss1 pbss2 pbss
+           end
+    in go pbss1' pbss2' pbss
+  let min_unsat_cores env defs : bformula =
+    let len = Array.length defs in
+    let pbss1 =
+      min_unsat_cores
+        defs
+        env
+        Pbss.empty (* pbss1' *)
+        Pbss.empty (* pbss2' *)
+        (Pbss.of_list (List.init len ~f:(Conj.singleton len)))
+    in
+    Pbss.to_list pbss1
+    |> List.map ~f:(Conj.bvalue defs)
+    |> Formula.mk_ors
+
+end
+
 let min_unsat_cores' : Formula.t list -> bformula =
   fun preds ->
-    let pbs =
-      List.mapi preds ~f:begin fun i pred ->
-        let p = of_formula pred in
-        let x = Fpat.(Formula.of_term
-                      @@ Term.mk_var (Idnt.make ("x"^string_of_int i))) in
-                      (* NOTE: ith predicate is named "xi" *)
-        (p, x)
-      end
-    in
-    Fpat.PredAbst.min_unsat_cores [] pbs
-    |> to_bformula
+    if false then
+      let pbs =
+        List.mapi preds ~f:begin fun i pred ->
+          let p = of_formula pred in
+          let x = Fpat.(Formula.of_term
+                        @@ Term.mk_var (Idnt.make ("x"^string_of_int i))) in
+                        (* NOTE: ith predicate is named "xi" *)
+          (p, x)
+        end
+      in
+      Fpat.PredAbst.min_unsat_cores [] pbs
+      |> to_bformula
+    else
+      let defs = Array.of_list @@
+        List.mapi preds ~f:begin fun i pred ->
+          let bvar = ("x"^string_of_int i, `Pos) in
+          FpatReImpl.{pred; bvar}
+        end
+      in
+      FpatReImpl.min_unsat_cores (Formula.Bool true) defs
 
 (* phiをpredsのみで（否定を使わずに）近似．弱い方に倒す *)
 let weakest_pre_cond' : Formula.t -> Formula.t list -> bformula =
   fun phi preds ->
-    let env = [] in
-    let pbs =
-      List.mapi preds ~f:begin fun i pred ->
-        let p = of_formula pred in
-        let x = Fpat.(Formula.of_term
-                      @@ Term.mk_var (Idnt.make ("x"^string_of_int i))) in
-                      (* NOTE: ith predicate is named "xi" *)
-        (p, x)
-      end
-    in
-    phi
-    |> of_formula
-    |> Fpat.PredAbst.weakest env pbs
-    |> fst
-    |> to_bformula
+    if false then
+      let env = [] in
+      let pbs =
+        List.mapi preds ~f:begin fun i pred ->
+          let p = of_formula pred in
+          let x = Fpat.(Formula.of_term
+                        @@ Term.mk_var (Idnt.make ("x"^string_of_int i))) in
+                        (* NOTE: ith predicate is named "xi" *)
+          (p, x)
+        end
+      in
+      phi
+      |> of_formula
+      |> Fpat.PredAbst.weakest env pbs
+      |> fst
+      |> to_bformula
+    else
+      let defs = Array.of_list @@
+        List.mapi preds ~f:begin fun i pred ->
+          let bvar = ("x"^string_of_int i, `Pos) in
+          FpatReImpl.{pred; bvar}
+        end
+      in
+      FpatReImpl.weakest (Formula.Bool true) defs phi
+
 
 (* phiをpredsのみで（否定を使わずに）近似．強い方に倒す *)
 (* TODO:
@@ -155,12 +435,14 @@ let aux_in_DNF : bformula -> int list list =
         | _ -> assert false
       end
     end
-let weakest_pre_cond : Formula.t -> Formula.t list -> int list list =
-  fun phi preds -> aux_in_DNF @@ weakest_pre_cond' phi preds
-let strongest_post_cond : Formula.t -> Formula.t list -> int list list =
-  fun phi preds -> aux_in_DNF @@ strongest_post_cond' phi preds
-let min_unsat_cores : Formula.t list -> int list list =
-  fun preds -> aux_in_DNF @@ min_unsat_cores' preds
-let min_valid_cores : Formula.t list -> int list list =
-  fun preds -> aux_in_DNF @@ min_valid_cores' preds
+let weakest_pre_cond : Formula.t -> Formula.t array -> int list list =
+  (* fun phi preds -> aux_in_DNF @@ weakest_pre_cond' phi @@ Array.to_list preds *)
+  fun phi preds -> aux_in_DNF @@ weakest_pre_cond' phi @@ Array.to_list preds
+
+let strongest_post_cond : Formula.t -> Formula.t array -> int list list =
+  fun phi preds -> aux_in_DNF @@ strongest_post_cond' phi @@ Array.to_list preds
+let min_unsat_cores : Formula.t array -> int list list =
+  fun preds -> aux_in_DNF @@ min_unsat_cores' @@ Array.to_list preds
+let min_valid_cores : Formula.t array -> int list list =
+  fun preds -> aux_in_DNF @@ min_valid_cores' @@ Array.to_list preds
 
