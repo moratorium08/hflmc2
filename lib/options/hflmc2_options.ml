652790d958e4cef503438a5f85527f716312778c
Hogeyama <gan13027830@gmail.com>
2019-12-02 15:59:50 +0900

    Improve --help

--- lib/options/hflmc2_options.ml
+++ lib/options/hflmc2_options.ml
@@ -14,9 +14,7 @@ end
 module Abstraction = struct
   (* NOTE Actual value is set by Cmdliner *)
   let max_I                = ref (Obj.magic())
-  (* let max_ors              = ref (Obj.magic()) *)
   let max_ands             = ref (Obj.magic())
-  let exhaustive_search    = ref (Obj.magic())
   let modify_pred_by_guard = ref (Obj.magic())
 end
 
@@ -45,43 +43,34 @@ let set_module_log_level level modules =
 (******************************************************************************)
 
 type params =
-  (* { input : string [@pos 0] [@docv "FILE"] *)
   { input : string list [@pos 0] [@docv "FILE"]
-
-  (* Logging *)
-  ; log_debug : string list [@default []] [@docv "MODULE,..."] [@aka ["debug"]]
-    (** Enable debug log of modules *)
-  ; log_info : string list [@default []] [@docv "MODULE,..."]
-    (** Enable info log of modules *)
 
   (* Whole procedure *)
   ; oneshot : bool [@default false]
-    (** Do not loop *)
+    (** Stop execution after first CEGAR loop *)
 
   (* Preprocess *)
   ; no_inlining : bool [@default false]
     (** Disable inlining *)
 
-  (*
-  (* Abstraction *)
-  ; abst_max_ors : int [@default 10]
-    (** Maximum number of disjunction in predicate abstraction *)
-  *)
+  (* Logging *)
+  ; log_debug : string list [@default []] [@docs "Log"] [@docv "MODULE,..."] [@aka ["debug"]]
+    (** Enable debug log of modules *)
+  ; log_info  : string list [@default []] [@docs "Log"] [@docv "MODULE,..."]
+    (** Enable info log of modules *)
 
-  ; abst_max_I : int [@default 2]
+  (* Abstraction *)
+  ; abst_max_I : int [@default 2] [@docs "Abstraction"]
     (** Maximum number of conjunction in the assumption of predicate abstraction *)
 
-  ; abst_max_ands : int [@default 10]
+  ; abst_max_ands : int [@default 10] [@docs "Abstraction"]
     (** Maximum number of conjunction in predicate abstraction *)
 
-  ; abst_exhaustive_search : bool [@default false]
-    (** Do exhaustive search in predicate abstraction *)
+  ; abst_no_modify_pred_by_guard : bool [@default false] [@docs "Abstraction"]
+    (** Do not modify [pred] into [C => pred] *)
 
-  ; abst_no_modify_pred_by_guard : bool [@default false]
-    (** Whether modify [pred] into [C => pred] *)
-
-  (* Abstraction *)
-  ; refine_legacy : bool [@default false]
+  (* Refine *)
+  ; refine_legacy : bool [@default false] [@docs "Refine"]
     (** Use old refine algorithm *)
 
   }
@@ -94,7 +83,6 @@ let set_up_params params =
   set_ref Preprocess.inlining              (not params.no_inlining);
   set_ref Abstraction.max_I                params.abst_max_I;
   set_ref Abstraction.max_ands             params.abst_max_ands;
-  set_ref Abstraction.exhaustive_search    params.abst_exhaustive_search;
   set_ref Abstraction.modify_pred_by_guard (not params.abst_no_modify_pred_by_guard);
   set_ref Refine.use_legacy                params.refine_legacy;
   params.input
