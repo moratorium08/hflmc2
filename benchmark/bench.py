9df67274300318ab6966ea47bd55db6903fcd0fb
Hiroyuki Katsura <katsura@kb.is.s.u-tokyo.ac.jp>
2019-12-16 18:46:58 +0900

    Extract common code

--- benchmark/bench.py
+++ benchmark/bench.py
@@ -3,27 +3,38 @@ import os
 import signal
 import subprocess
 import json
+import time
 
-TARGET = 'dune exec hflmc2 -- '
-TIMEOUT = 5  # sec
+from common import *
+from target import cli_arg, gen_cmd, parse_stdout, config, callback, stat
+
+TIMEOUT = 5
+
+class Config:
+    def __init__(self):
+        pass
 
 parser = argparse.ArgumentParser()
 parser.add_argument("list", help="list which contains benchmarks")
 parser.add_argument("--timeout", help="timeout", default=TIMEOUT, type=int)
-parser.add_argument('--no-inline', action='store_true')
 parser.add_argument('--json', help="set filename in which results will be saved", default=None)
-parser.add_argument("--solver", help="set background CHC solver", default="auto")
 parser.add_argument("--basedir", help="base directory", default="./inputs/")
+parser = cli_arg(parser)
 args = parser.parse_args()
 
-cmd_template = TARGET + ' {} {}'  # <option> <filename>
+cfg = Config()
+cfg.args = args
+config(cfg)
 
 
 def run(cmd):
+    st = time.perf_counter()
     with subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, preexec_fn=os.setsid) as p:
         try:
             output, _ = p.communicate(timeout=args.timeout)
-            return output
+            ed = time.perf_counter()
+            elapsed = ed - st
+            return output, elapsed
         except subprocess.TimeoutExpired:
             try:
                 os.killpg(p.pid, signal.SIGKILL)
@@ -32,137 +43,39 @@ def run(cmd):
             raise
 
 
-def gen_cmd(file):
-    ags = []
-    if args.no_inline:
-        ags.append('--no-inlining')
-    s = args.solver
-    if s == 'z3':
-        ags.append("--solver z3")
-    elif s == 'hoice':
-        ags.append("--solver hoice")
-    elif s == 'fptprove':
-        ags.append("--solver fptprove")
-    elif s == 'auto':
-        ags.append("--solver auto")
-    else:
-        raise Exception('No such solver')
-
-    ag = ' '.join(ags)
-    return cmd_template.format(ag, file)
-
-
-
-class ParseError(Exception):
-    pass
-
-
-def parse_stdout(stdout):
-    idx = stdout.find('Verification Result:')
-    result = stdout[idx:].split('\n')
-    cur = 0
-
-    def readline():
-        nonlocal cur
-        line = result[cur]
-        cur += 1
-        return line
-
-    def parse_verification_result():
-        line = readline()
-        if 'Invalid' in line:
-            return 'invalid'
-        elif 'Valid' in line:
-            return 'valid'
-        elif 'Fail' in line:
-            return 'fail'
-        else:
-            raise ParseError
-
-    def parse_profile():
-        line = readline()
-        if 'total' in line:
-            return float(line.replace(' ', '').replace('sec', '').split(':')[1])
-        else:
-            return parse_profile()
-
-    result_data = dict()
-    result_data['solver'] = 'fptprove' if 'Some definite clause has or-head' in stdout else 'z3'
-    while cur < len(result):
-        line = readline()
-        if 'Verification Result:' in line:
-            result_data['result'] = parse_verification_result()
-        elif 'Profiling' in line:
-            result_data['total'] = parse_profile()
-        elif 'ill-typed' in line:
-            result_data['ok'] = False
-            result_data['error'] = 'ill-typed'
-            return result_data
-        else:
-            pass
-    # adhoc
-    if 'result' in result_data and 'total' in result_data:
-        result_data['ok'] = True
-    else:
-        result_data['error'] = 'unknown'
-        result_data['ok'] = False
-    return result_data
-
-
 def p(file, result):
-    if result['ok']:
-        print(f'{file}\t{result["result"]}\t{result["total"]}\t{result["solver"]}')
-    else:
-        print(f'{file}\t{result["error"]}\t{TIMEOUT}\t{result["solver"]}')
-
-
-def handle(file, callback=p):
-    cmd = gen_cmd(file)
-    try:
-        stdout = run(cmd).decode('utf-8')
-        result = parse_stdout(stdout)
-    except subprocess.TimeoutExpired:
-        result = {'ok': False, 'error': 'timeout'}
-    callback(file, result)
+    print(result)
 
 
 results = []
-
-
-def callback(file, result):
-    p(file, result)
+def handle(file, parser, callback=p):
+    cmd = gen_cmd(file)
+    try:
+        stdout, t = run(cmd)
+        stdout = stdout.decode('utf-8')
+        result = parser(stdout)
+        result['time'] = t
+    except subprocess.TimeoutExpired:
+        result = {'ok': False, 'error': 'timeout'}
+        result['time'] = args.timeout
+    if 'result' not in result:
+        result['result'] = 'fail'
     result['file'] = file
+    callback(file, result)
     results.append(result)
-
-
-def stat():
-    valid_cnt = sum(1 for _ in filter(
-        lambda x: 'result' in x and x['result'] == 'valid', results))
-    invalid_cnt = sum(1 for _ in filter(
-        lambda x: 'result' in x and x['result'] == 'invalid', results))
-    timeout_cnt = sum(1 for _ in filter(
-        lambda x: 'error' in x and x['error'] == 'timeout', results))
-
-    no_errors = [x for x in results if x['ok']]
-    mean = sum(x['total'] for x in no_errors) / len(no_errors)
-    print('[Result]')
-    print(f'- solver={args.solver}')
-    print(f'- valid={valid_cnt}')
-    print(f'- invalid={invalid_cnt}')
-    print(f'- timeout={timeout_cnt}')
-    print(f'- mean_without_errors={mean}')
 
 
 def save_json(filename):
     with open(filename, "w") as f:
         json.dump(results, f)
 
+
 def main():
     with open(args.list) as f:
         files = f.read().strip('\n').split('\n')
     for file in files:
-        handle(os.path.join(args.basedir, file), callback=callback)
-    stat()
+        handle(os.path.join(args.basedir, file), parse_stdout, callback=callback)
+    stat(results)
     if args.json is not None:
         save_json(args.json)
 
---
+++ benchmark/target.py
@@ -0,0 +1,118 @@
+from common import *
+
+TARGET = 'dune exec hflmc2 -- '
+cmd_template = TARGET + ' {} {}'  # <option> <filename>
+
+cfg = None
+
+def config(c):
+    global cfg
+    cfg = c
+
+
+def cli_arg(parser):
+    parser.add_argument("--solver", help="set background CHC solver", default="auto")
+    parser.add_argument('--no-inline', action='store_true')
+    return parser
+
+
+def gen_cmd(file):
+    args = cfg.args
+    ags = []
+    if args.no_inline:
+        ags.append('--no-inlining')
+    s = args.solver
+    if s == 'z3':
+        ags.append("--solver z3")
+    elif s == 'hoice':
+        ags.append("--solver hoice")
+    elif s == 'fptprove':
+        ags.append("--solver fptprove")
+    elif s == 'auto':
+        ags.append("--solver auto")
+    else:
+        raise Exception('No such solver')
+
+    ag = ' '.join(ags)
+    return cmd_template.format(ag, file)
+
+
+def parse_stdout(stdout):
+    idx = stdout.find('Verification Result:')
+    result = stdout[idx:].split('\n')
+    cur = 0
+
+    def readline():
+        nonlocal cur
+        line = result[cur]
+        cur += 1
+        return line
+
+    def parse_verification_result():
+        line = readline()
+        if 'Invalid' in line:
+            return 'invalid'
+        elif 'Valid' in line:
+            return 'valid'
+        elif 'Fail' in line:
+            return 'fail'
+        else:
+            raise ParseError
+
+    def parse_profile():
+        line = readline()
+        if 'total' in line:
+            return float(line.replace(' ', '').replace('sec', '').split(':')[1])
+        else:
+            return parse_profile()
+
+    result_data = dict()
+    result_data['solver'] = 'fptprove' if 'Some definite clause has or-head' in stdout else 'z3'
+    while cur < len(result):
+        line = readline()
+        if 'Verification Result:' in line:
+            result_data['result'] = parse_verification_result()
+        elif 'Profiling' in line:
+            result_data['total'] = parse_profile()
+        elif 'ill-typed' in line:
+            result_data['ok'] = False
+            result_data['error'] = 'ill-typed'
+            return result_data
+        else:
+            pass
+    # adhoc
+    if 'result' in result_data and 'total' in result_data:
+        result_data['ok'] = True
+    else:
+        result_data['error'] = 'unknown'
+        result_data['ok'] = False
+    return result_data
+
+
+def p(file, result):
+    if result['ok']:
+        print(f'{file}\t{result["result"]}\t{result["total"]}\t{result["solver"]}')
+    else:
+        print(f'{file}\t{result["error"]}\t{cfg.args.timeout}\t{result["solver"]}')
+
+
+def callback(file, result):
+    p(file, result)
+
+
+def stat(args, results):
+    valid_cnt = sum(1 for _ in filter(
+        lambda x: 'result' in x and x['result'] == 'valid', results))
+    invalid_cnt = sum(1 for _ in filter(
+        lambda x: 'result' in x and x['result'] == 'invalid', results))
+    timeout_cnt = sum(1 for _ in filter(
+        lambda x: 'error' in x and x['error'] == 'timeout', results))
+
+    no_errors = [x for x in results if x['ok']]
+    mean = sum(x['total'] for x in no_errors) / len(no_errors)
+    print('[Result]')
+    print(f'- solver={args.solver}')
+    print(f'- valid={valid_cnt}')
+    print(f'- invalid={invalid_cnt}')
+    print(f'- timeout={timeout_cnt}')
+    print(f'- mean_without_errors={mean}')
