9c900ea1ae3e54a6cdb0b191d832247677d11654
Hiroyuki Katsura <katsura@kb.is.s.u-tokyo.ac.jp>
2019-12-16 18:54:54 +0900

    Fix iwayama san's bench

---
+++ scripts/iwa_hflmc2.py
@@ -0,0 +1,71 @@
+from common import *
+
+TARGET = 'dune exec hflmc2 -- '
+cmd_template = TARGET + ' {} {}'  # <option> <filename>
+
+cfg = None
+
+def config(c):
+    global cfg
+    cfg = c
+
+
+def cli_arg(parser):
+    parser.add_argument('--no-inline', action='store_true')
+    return parser
+
+
+def gen_cmd(file):
+    args = cfg.args
+    ags = []
+    if args.no_inline:
+        ags.append('--no-inlining')
+
+    ag = ' '.join(ags)
+    return cmd_template.format(ag, file)
+
+
+def parse_stdout(stdout):
+    idx = stdout.find('Verification Result:')
+    result = stdout[idx:].split('\n')
+    cur = 0
+
+    def readline():
+        nonlocal cur
+        line = result[cur]
+        cur += 1
+        return line
+
+    def parse_verification_result():
+        line = readline()
+        if 'Invalid' in line:
+            return 'invalid'
+        elif 'Valid' in line:
+            return 'valid'
+        elif 'Fail' in line:
+            return 'fail'
+        else:
+            raise ParseError
+
+    result_data = dict()
+    while cur < len(result):
+        line = readline()
+        if 'Verification Result:' in line:
+            result_data['result'] = parse_verification_result()
+        elif 'ill-typed' in line:
+            result_data['ok'] = False
+            result_data['error'] = 'ill-typed'
+            return result_data
+        else:
+            pass
+    if 'result' not in result_data:
+        result_data['error'] = 'unknown'
+    return result_data
+
+
+def callback(file, result):
+    print(file)
+
+
+def stat(results):
+    pass
