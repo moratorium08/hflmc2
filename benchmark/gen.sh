4eefa0484b5141ca787c1936b18221040953a9e0
Hiroyuki Katsura <katsura@kb.is.s.u-tokyo.ac.jp>
2019-12-18 18:45:45 +0900

    Fix benchmark

--- benchmark/gen.sh
+++ benchmark/gen.sh
@@ -9,11 +9,12 @@ rm -f ../lists/all
 
 for l in $LISTS
 do
-    find $l -depth 1 | grep -E ".*in" > ../lists/$l
-    find $l -depth 1 | grep -E ".*in" >> ../lists/all
+    find $l -depth 1 | grep -E ".*in$" > ../lists/$l
+    find $l -depth 1 | grep -E ".*in$" >> ../lists/all
 done
 
 cd ../
 # adhoc
 mv lists/Burn_POPL18 lists/burn
 mv lists/higher_nonterminating lists/nonterminating
+cat lists/mochi lists/mochi2 > lists/mochis
