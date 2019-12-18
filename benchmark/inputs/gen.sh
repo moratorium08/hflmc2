ad7afabc4d102abee7d00cb7176838aeba1cc9a1
Hiroyuki Katsura <katsura@kb.is.s.u-tokyo.ac.jp>
2019-12-18 14:44:52 +0900

    Fix lists

---
+++ benchmark/inputs/gen.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+LISTS=`ls  | grep -E "[^n]$"`
+
+mkdir -p lists
+rm -f lists/all
+
+for l in $LISTS
+do
+    find $l -depth 1 | grep -E ".*in" > lists/$l
+    find $l -depth 1 | grep -E ".*in" >> lists/all
+done
+
+# adhoc
+mv lists/Burn_POPL18 lists/burn
