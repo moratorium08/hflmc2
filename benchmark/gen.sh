077a53fe0aa8ef5eaac4d9712171adaa12b63f2f
Hiroyuki Katsura <katsura@kb.is.s.u-tokyo.ac.jp>
2020-01-20 21:09:54 +0900

    Add horus

--- benchmark/gen.sh
+++ benchmark/gen.sh
@@ -19,3 +19,18 @@ mv lists/Burn_POPL18 lists/burn
 mv lists/Burn_POPL18_2 lists/burn2
 mv lists/higher_nonterminating lists/nonterminating
 cat lists/mochi lists/mochi2 > lists/mochis
+
+
+############ HOCHC ##################
+
+cd hochcs
+LISTS=`ls  | grep -E "[^n]$"`
+mkdir -p ../lists
+rm -f ../lists/horus_all
+
+for l in $LISTS
+do
+    find $l -depth 1 | grep -E ".*inp$" > ../lists/$l
+    find $l -depth 1 | grep -E ".*inp$" >> ../lists/horusall
+done
+cd ../
