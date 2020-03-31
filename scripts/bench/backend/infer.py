ad379065689f3b2608ec70c5ebf09c3ba1800923
Hiroyuki Katsura <katsura@kb.is.s.u-tokyo.ac.jp>
2020-03-31 11:53:03 +0900

    Add two backends

---
+++ scripts/bench/backend/infer.py
@@ -0,0 +1,46 @@
+############ Do not change[start] ############
+from common import *
+############  Do not change[end]  ############
+
+TARGET = 'infer'
+cmd_template = TARGET + ' {} {}'  # <option> <filename>
+
+cfg = None
+
+
+def pre_cmd():
+    return ''
+
+
+def config(c):
+    global cfg
+    cfg = c
+
+
+def cli_arg(parser):
+    parser.add_argument('--no-inline', action='store_true')
+    return parser
+
+
+def gen_cmd(file):
+    args = cfg.args
+    ags = []
+    if args.no_inline:
+        ags.append('--no-inlining')
+
+    ag = ' '.join(ags)
+    return cmd_template.format(ag, file)
+
+
+def parse_stdout(stdout):
+    result_data = dict()
+    result_data['result'] = 'invalid' if 'Invalid' in stdout else 'valid' if 'Valid' in stdout else 'fail'
+    return result_data
+
+
+def callback(file, result):
+    print(file)
+
+
+def stat(results):
+    print(results)
---
+++ scripts/bench/backend/mochi.py
@@ -0,0 +1,39 @@
+############ Do not change[start] ############
+from common import *
+############  Do not change[end]  ############
+
+TARGET = 'mochi -only-result'
+cmd_template = TARGET + ' {}'  # <option> <filename>
+
+cfg = None
+
+
+def pre_cmd():
+    return ''
+
+
+def config(c):
+    global cfg
+    cfg = c
+
+
+def cli_arg(parser):
+    return parser
+
+
+def gen_cmd(file):
+    return cmd_template.format(file)
+
+
+def parse_stdout(stdout):
+    result_data = dict()
+    result_data['result'] = 'invalid' if 'Unsafe' in stdout else 'valid' if 'Safe' in stdout else 'fail'
+    return result_data
+
+
+def callback(file, result):
+    print(file)
+
+
+def stat(results):
+    print(results)
