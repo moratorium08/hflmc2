47fafd60d702935168661ad09ae6a2be0736c2fc
katsura <katsura@kb.is.s.u-tokyo.ac.jp>
2022-10-12 11:48:57 +0900

    Fix strange type error

--- lib/util/hflmc2_util.ml
+++ lib/util/hflmc2_util.ml
@@ -182,10 +182,8 @@ module Fn = struct
   let neg i = -i
   let const x _ = x
 
-  let print ?(tag="") pp x =
-    if tag = ""
-    then Format.printf "@[%a@]@." pp x
-    else Format.printf "%s: @[%a@]@." tag pp x
+  let print tag pp x =
+    Format.printf "%s: @[%a@]@." tag pp x
 
   let on injection g x y = g (injection x) (injection y)
 
